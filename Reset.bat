296e1d94fd3a72daa7f72eea0d0f7d735b585c9f
Aditya <901aditya109@gmail.com>
2024-11-16 12:42:13 +0530

    Add status indicators to Reset.bat

--- Reset.bat
+++ Reset.bat
@@ -1,11 +1,13 @@
 @cls
 @set /P choice="THIS WILL DELETE ALL MoneyBat DATA! DO YOU WISH TO CONTINUE? (y/n) "
 @if %choice% == y (
+@echo [*] Removing logs..
 @rmdir logs /s /q
+@echo [*] Removing wallet..
 @del Wallet
 @echo Done.. Run Setup.bat to reconfigure MoneyBat
 @TIMEOUT /T 7
 ) else (
-@echo Exiting..
+@echo Exiting.. No changes made
 @TIMEOUT /T 7
 )
