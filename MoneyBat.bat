6ce8801926781cba1f7e809e6e484c98b4da9df9
Aditya <901aditya109@gmail.com>
2024-11-16 12:42:13 +0530

    Add features for overspending

--- MoneyBat.bat
+++ MoneyBat.bat
@@ -4,13 +4,18 @@
 @set /P budget=<budget.MoneyBat
 @set /A remaining = %budget% - %spent%
 @echo %remaining% > budget.MoneyBat
+@if %remaining% lss 0 @echo [!] Overspent
 
 @echo ---- >> MoneyBat.log
 @echo Date: %DATE% >> MoneyBat.log
 @echo Budget: %budget% >> MoneyBat.log
 @echo Spent: %spent% >> MoneyBat.log
 @echo For: %purpose% >> MoneyBat.log
-@echo Remaining: %remaining% >> MoneyBat.log
+@if %remaining% lss 0 (
+	@echo Remaining: %remaining% [Overspent] >> MoneyBat.log
+) else (
+	@echo Remaining: %remaining% >> MoneyBat.log
+)
 
 @echo [*] Logs updated
 @TIMEOUT /T 10
