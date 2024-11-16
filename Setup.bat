52fa08839c03ffbbb83466df7d896e0473383b98
Aditya <901aditya109@gmail.com>
2024-11-16 12:42:14 +0530

    Add status indicators for Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,7 +1,10 @@
 @cls
 @set /P budget="Budget? "
+@echo [*] Creating wallet..
 @echo %budget% > Wallet
+@echo [*] Making log directory..
 @mkdir logs
+@echo
 @echo Setup is done.. You can safely exit this window. DO NOT RUN SETUP AGAIN AS IT WILL RESET YOUR BUDGET. If you ever feel the need to reset moneybat completely, just run Reset.bat, and all variables will be set to default values.
 @TIMEOUT /T 60
 
