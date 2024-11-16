d3933babdc3b70c3ba9083ee3ade71f9120946af
Aditya <901aditya109@gmail.com>
2024-11-16 12:42:14 +0530

    Remove echo bug in Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -4,7 +4,6 @@
 @echo %budget% > Wallet
 @echo [*] Making log directory..
 @mkdir logs
-@echo
 @echo Setup is done.. You can safely exit this window. DO NOT RUN SETUP AGAIN AS IT WILL RESET YOUR BUDGET. If you ever feel the need to reset moneybat completely, just run Reset.bat, and all variables will be set to default values.
 @TIMEOUT /T 60
 
