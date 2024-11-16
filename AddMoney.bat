fa87bcf0f3fb1c5db9176e8f577f6d8f01ee0c0f
Aditya <901aditya109@gmail.com>
2024-11-16 12:42:13 +0530

    Add logging of job to AddMoney.bat

--- AddMoney.bat
+++ AddMoney.bat
@@ -1,10 +1,11 @@
 @cls
 @set /P budget="Amount to add to existing budget? "
+@set /P job="Job: "
 @set /P oldbudget=<budget.MoneyBat
 @set /A finalbudget = %budget% + %oldbudget%
 @echo %finalbudget% > budget.MoneyBat
 @echo ---- >> MoneyBat.log
-@echo Added %budget% on %DATE% >> MoneyBat.log
+@echo Added %budget% on %DATE% [%job%] >> MoneyBat.log
 @echo [*] Added %budget% on %DATE% at %TIME%
 @TIMEOUT /T 10
 
